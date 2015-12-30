286bfde939b09e8ac8d2c18237d6301f9f517d82
Saturngod <saturngod@gmail.com>
2015-12-30 11:46:54 +0800

    add show preview on/off

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 1024, height: 720});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
   //mainWindow.openDevTools();
